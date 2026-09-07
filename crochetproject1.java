3d6f9d6110535c725cc8b706a1e0d1bded64cfad
Auliaaa0920 <auliazahraanindia00@gmail.com>
2026-09-08 00:04:44 +0700

    crochetproject1.java

--- crochetproject1.java
+++ crochetproject1.java
@@ -9,5 +9,46 @@ package com.mycompany;
  * @author AULIA
  */
 public class crochetproject1 {
-    
+public static void main(String[] args) {
+        RumusCrochet ch = new RumusCrochet(
+                "Chain",
+                "CH",
+                "Rantai",
+                "Tusuk dasar untuk membuat rantai pada crochet."
+        );
+
+        RumusCrochet sc = new RumusCrochet(
+                "Single Crochet",
+                "SC",
+                "Tusuk Tunggal",
+                "Tusuk dasar yang menghasilkan rajutan cukup rapat."
+        );
+
+        RumusCrochet hdc = new RumusCrochet(
+                "Half Double Crochet",
+                "HDC",
+                "Setengah Tusuk Ganda",
+                "Tusuk dengan tinggi di antara SC dan DC."
+        );
+
+        RumusCrochet dc = new RumusCrochet(
+                "Double Crochet",
+                "DC",
+                "Tusuk Ganda",
+                "Tusuk yang lebih tinggi dibandingkan SC dan HDC."
+        );
+
+        RumusCrochet slst = new RumusCrochet(
+                "Slip Stitch",
+                "SL ST",
+                "Tusuk Selip",
+                "Tusuk yang digunakan untuk menyambung atau berpindah posisi."
+        );
+
+        ch.tampilkanInfo();
+        sc.tampilkanInfo();
+        hdc.tampilkanInfo();
+        dc.tampilkanInfo();
+        slst.tampilkanInfo();
+    }  
 }
