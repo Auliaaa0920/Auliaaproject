eedabc77d76eccd89f1bdafbb8e25bb12694faed
Auliaaa0920 <auliazahraanindia00@gmail.com>
2026-09-14 13:45:12 +0700

    Netbeans

---
+++ crochetproject1/src/main/java/com/mycompany/crochetproject1/Crochetproject1.java
@@ -0,0 +1,58 @@
+/*
+
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+  */
+
+/**
+*
+
+* @author AULIA
+  */
+  package com.mycompany.crochetproject1;
+
+public class Crochetproject1 {
+
+public static void main(String[] args) {
+
+    RumusCrochet ch = new RumusCrochet(
+            "Chain",
+            "CH",
+            "Rantai",
+            "Tusuk dasar untuk membuat rantai pada crochet."
+    );
+
+    RumusCrochet sc = new RumusCrochet(
+            "Single Crochet",
+            "SC",
+            "Tusuk Tunggal",
+            "Tusuk dasar yang menghasilkan rajutan cukup rapat."
+    );
+
+    RumusCrochet hdc = new RumusCrochet(
+            "Half Double Crochet",
+            "HDC",
+            "Setengah Tusuk Ganda",
+            "Tusuk dengan tinggi di antara SC dan DC."
+    );
+
+    RumusCrochet dc = new RumusCrochet(
+            "Double Crochet",
+            "DC",
+            "Tusuk Ganda",
+            "Tusuk yang lebih tinggi dibandingkan SC dan HDC."
+    );
+
+    RumusCrochet slst = new RumusCrochet(
+            "Slip Stitch",
+            "SL ST",
+            "Tusuk Selip",
+            "Tusuk yang digunakan untuk menyambung atau berpindah posisi."
+    );
+
+    ch.tampilkanInfo();
+    sc.tampilkanInfo();
+    hdc.tampilkanInfo();
+    dc.tampilkanInfo();
+    slst.tampilkanInfo();
+}
+}
